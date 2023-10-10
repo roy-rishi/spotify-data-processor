cff7de7c0e0b124e81090c31e5c4db80e4569c5c
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2023-10-09 21:05:26 -0700

    reformat to specify utc timezone
    logs from spotify are in utc, so edit timezones to reflect that and match logger format

--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,7 @@
 import json
+from datetime import datetime, timezone
 
-dataPath = input("path to file for preprocessing: ")
+TARGET_FORMAT="%Y-%m-%d %H:%M:%S %z"
 
 def readLogs():
     logFile = open(dataPath)
@@ -13,12 +14,29 @@ def writeLogs(logs):
     json.dump(logs, logFile, indent=2)
     logFile.close()
 
+def reformat_utc(date_string):
+    date_pdt = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+    print(f"{date_pdt} PDT")
 
+    utc_str = date_pdt.strftime(TARGET_FORMAT + "+0000 UTC")
+    print(utc_str)
+    return utc_str
+
+
+dataPath = input("path to file for preprocessing: ")
 logs = readLogs()
 
+# convert artist name strings to single element arrays of one string
+# "Lorde" -> ["Lorde"]
 for i in range(len(logs)):
     name = logs[i]["master_metadata_album_artist_name"]
     if type(name) is str:
         logs[i]["master_metadata_album_artist_name"] = [name]
+
+# reformat UTC
+# %Y-%m-%dT%H:%M:%SZ  ->  %Y-%m-%d %H:%M:%S %z %Z
+for i in range(len(logs)):
+    date_str = logs[i]["ts"]
+    logs[i]["ts"] = reformat_utc(date_str)
 
 writeLogs(logs)
